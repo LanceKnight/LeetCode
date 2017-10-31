11d6bea20582cddf63de65a8d3d4867dc22b59b7
Lance Liu <lanceknight@users.noreply.github.com>
2017-10-31 13:15:34 -0500

    Keyboard Row V1.0
    Q. 500

--- main.py
+++ main.py
@@ -1,46 +1,27 @@
-# Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-    def build(self, val, left, right):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return str(self.val) + " " + str(self.left) + " " + str(self.right)
-
-
-def constructMaximumBinaryTree(nums):
+#500
+def findWords( words):
     """
-    :type nums: List[int]
-    :rtype: TreeNode
+    :type words: List[str]
+    :rtype: List[str]
     """
+    list = []
+    for word in words:
+        is_row1 = True
+        is_row2 = True
+        is_row3 = True
+        #check row 1
+        for char in word:
+            if (char not in 'qwertyuiopQWERTYUIOP'):
+                is_row1 = False
 
-    root = TreeNode(nums.index(max(nums)))
-    #print "nums:", nums
-    #print "nums.index(max(nums))", nums.index(max(nums))
-    #print "max", max(nums)
-    root.val = max(nums)
-    if((nums.index(max(nums))) !=0 ):
-        #print "here in the left side"
-        #print "nums[0:nums.index(max(nums))]",nums[0:nums.index(max(nums))]
-        root.left = constructMaximumBinaryTree(nums[0:nums.index(max(nums))])
-    else:
-        #print "left assign none"
-        root.left = None
-    if(nums.index(max(nums)) != len(nums)-1):
-        #print "here in the right side"
-        root.right = constructMaximumBinaryTree(nums[nums.index(max(nums))+1:])
-    else:
-        root.right = None
-        #print "right assign none"
-    #print "\n"
-    return root
+            if (char not in 'asdfghjklASDFGHJKL'):
+                is_row2 = False
 
+            if (char not in 'zxcvbnmZXCVBNM'):
+                is_row3 = False
 
+        if (is_row1 == True) or (is_row2 == True) or (is_row3 == True):
+            list.append(word)
+    return list
 
-print constructMaximumBinaryTree([3,2,1,6,0,5])
-
+print findWords(["Hello", "Alaska", "Dad", "Peace"])
