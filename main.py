1d1d1c2d3ee11d4a27a9c19b4dc2d9b0b75588cb
Lance Liu <lanceknight@users.noreply.github.com>
2017-11-13 13:29:37 -0600

    V1.0
    basic function

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-#29
-def divide( dividend, divisor):
-    if divisor == 0:
-        return "MAX_INT"
-    flag = divisor*dividend
-    dividend = abs(dividend)
-    divisor = abs(divisor)
-    left = dividend
-    count = 0
+#91
+def numDecodings(s):
+    #print "current s", s
+    """
+     :type s: str
+     :rtype: int
+     """
 
-    while left >= divisor:
-        left = left-divisor
-        count +=1
+    # print "current s", s
+    num = len(s)
+    if (num == 0):
+        return 0
+    if (num == 1):
+        if(int(s)!=0):
+            return 1
+        else:
+            return 0
+    if (num == 2):
+        # print "s[-1]", int(s[-1]) is not 0
+        if (int(s) <= 26) and (int(s[-2:]) is not 0):
+            if int(s[-1]) is not 0:
+                if (int(s)<10):
+                    return 0
+                else:
+                    return 2
+            else:
+                return 1
+        elif (int(s) >26):
+            if(int(s[-1])is not 0):
+                return 1
+            else:
+                return 0
+        elif (int(s) is 0):
+            return 0
+    # print "s[-2:]",s[-2:]
+    if (int(s[-2:]) <= 26) and (int(s[-2:]) is not 0):
+        # print s[:-1]
+        if (int(s[-1]) != 0):
+            if (int(s[-2:]) >= 10):
+                return numDecodings(s[:-1]) + numDecodings(s[:-2])
+            else:
+                return numDecodings(s[:-1])
+        else:
+            return numDecodings(s[:-2])
+    elif int(s[-2:]) >26:
+        if int(s[-1]) is not 0:
+            return numDecodings(s[:-1])
+        else:
+            return 0
 
-    if flag<0:
-        return -count
-    else:
-        return count
+    elif int(s[-2:]) ==0:
+        return 0
 
-
-
-7483648
-#print divide(-214111111,-1)
-a = 3
-a <<=1
-print a
+print numDecodings("4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948")
