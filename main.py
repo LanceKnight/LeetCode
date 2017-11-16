a2cc0951210d28b5c089d2933155537eddbadee8
Lance Liu <lanceknight@users.noreply.github.com>
2017-11-16 14:43:10 -0600

    #136 initial solution

--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-#165
-def compareVersion(version1, version2):
+#136
+
+def singleNumber(nums):
     """
-    :type version1: str
-    :type version2: str
+    :type nums: List[int]
     :rtype: int
     """
 
+    result = 0
+    for i in nums:
+        print "i:", i, " bin(i):", bin(i), " result:",bin(result)
 
-    v1 = version1.split(".")
-    v2 = version2.split(".")
-    i = 0
-    #print "len(v1):", len(v1), " len(v2):", len(v2)
-    if len(v1) < len(v2):
-        while i < len(v1):
-            head_v1 = int(v1[i])
-            head_v2 = int(v2[i])
-            if head_v1 > head_v2:
-                return 1
-            elif head_v1 < head_v2:
-                return -1
-            elif head_v1 == head_v2:
-                pass
-                i+=1
-        if int(v2[-1]) != 0:
-            return -1
-        else:
-            return 0
+        result ^= i
+    return result
 
-    elif len(v1) > len(v2):
-        while i < len(v2):
-            head_v1 = int(v1[i])
-            head_v2 = int(v2[i])
-            if head_v1 > head_v2:
-                return 1
-            elif head_v1 < head_v2:
-                return -1
-            elif head_v1 == head_v2:
-                pass
-                i+=1
-        if int(v1[-1]) != 0:
-            return 1
-        else:
-            return 0
-    elif len(v1) == len(v2):
-        while i < len(v2):
-            head_v1 = int(v1[i])
-            head_v2 = int(v2[i])
-            if head_v1 > head_v2:
-                return 1
-            elif head_v1 < head_v2:
-                return -1
-            elif head_v1 == head_v2:
-                pass
-                i+=1
-        return 0
-
-
-
-print compareVersion("1.0.0.1","1.1")
+print singleNumber([])
