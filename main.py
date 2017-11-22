6a4f3ed082ee8e163ff62863551eee6683843789
Lance Liu <lanceknight@users.noreply.github.com>
2017-11-22 13:48:18 -0600

    #406 initial solution

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
-#137
-def singleNumber(nums):
+#406
+def reconstructQueue(people):
     """
-    :type nums: List[int]
-    :rtype: int
+    :type people: List[List[int]]
+    :rtype: List[List[int]]
     """
+    people.sort(key=lambda (h,k): (-h,k))
+    print people
+    new_list = []
+    for person in people:
+        new_list.insert(person[1],person)
+        print new_list
+    return new_list
 
-    a = 0
-    b = 0
-    for i in nums:
-        #print "i:", i, " bin(i):", bin(i), " result:",bin(result)
-
-        ta = (~a&b&i)|(a&~b&~i)
-        b = (~a&~b&i)|(~a&b&~i)
-        a = ta
-
-        print  i, "bin(a)", bin(a), "bin(b)", bin(b)
-    print  "bin(a)",bin(a),"bin(b)", bin(b), ~a&b
-    return ~a&b
-
-def test():
-    print "a b c a b"
-    for i in [0, 1]:
-        for a in [0,1]:
-            for b in [0,1]:
-                print a,b,i, (~a&b&i)|(a&~b&~i), (~a&~b&i)|(~a&b&~i)
-
-
-print singleNumber([2,2,2,5,5,4,5])
+print reconstructQueue([[9,0],[7,0],[1,9],[3,0],[2,7],[5,3],[6,0],[3,4],[6,2],[5,2]])
